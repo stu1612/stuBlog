47665a68ed6c0fdad2ca0a85487c001b04d7872c
Stu Bolderson <stu.bolderson@aol.com>
2024-01-23 18:20:39 +0100

    testing web share with no file

--- components/shared/WebShare.tsx
+++ components/shared/WebShare.tsx
@@ -6,31 +6,46 @@ export default function WebShare({ data }: any) {
   const onHandleShare = async () => {
     try {
       if (navigator.share) {
-        const imageUrl = data?.post?.image?.url;
+        await navigator.share({
+          title: data?.post?.title,
+          text: data?.post?.excerpt,
+          url: typeof window !== "undefined" ? window.location.href : "",
+        });
 
-        if (imageUrl) {
-          const response = await fetch(imageUrl);
-          const blob = await response.blob();
-          const file = new File([blob], "image.jpg", { type: "image/jpeg" });
-
-          await navigator.share({
-            title: data?.post?.title,
-            text: data?.post?.excerpt,
-            url: typeof window !== "undefined" ? window.location.href : "",
-            files: [file],
-          });
-
-          console.log("Successfully shared!");
-        } else {
-          console.error("Image URL not available");
-        }
-      } else {
-        console.error("Web Share API not supported");
+        console.log("Successfully shared!");
       }
     } catch (err) {
       console.error("Error sharing:", err);
     }
   };
+  // const onHandleShare = async () => {
+  //   try {
+  //     if (navigator.share) {
+  //       const imageUrl = data?.post?.image?.url;
+
+  //       if (imageUrl) {
+  //         const response = await fetch(imageUrl);
+  //         const blob = await response.blob();
+  //         const file = new File([blob], "image.jpg", { type: "image/jpeg" });
+
+  //         await navigator.share({
+  //           title: data?.post?.title,
+  //           text: data?.post?.excerpt,
+  //           url: typeof window !== "undefined" ? window.location.href : "",
+  //           files: [file],
+  //         });
+
+  //         console.log("Successfully shared!");
+  //       } else {
+  //         console.error("Image URL not available");
+  //       }
+  //     } else {
+  //       console.error("Web Share API not supported");
+  //     }
+  //   } catch (err) {
+  //     console.error("Error sharing:", err);
+  //   }
+  // };
 
   return <Button onClick={onHandleShare}>Share</Button>;
 }
